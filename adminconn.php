c751dd1ba5c51d262592ecf6c01451b52d6b699c
BiswajitRakshit7781 <142657794+BiswajitRakshit7781@users.noreply.github.com>
2023-11-01 14:55:30 +0530

    1.11

--- adminconn.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// arnab : arnabpal691@gmail.com
-// minakshi : minakshi24saha@gmail.com
-
-?>
--- login.php
+++ login.php
@@ -1,30 +1,44 @@
 <?php
 if($_SERVER["REQUEST_METHOD"]=="POST"){
   $email = $_POST['email'];
-  $post = $_POST['dropdown'];
+  $post = $_POST['post'];
   $password = $_POST['password'];
 
-  $host = "localhost";
-  $dbusername = "root";
-  $dbname = "project_database";
-  $dbpasword = " ";
+      $servername = "localhost";
+      $username = "root";
+      $password = "";
+      $database = "project_database";
 
-  $conn = new mysqli($host,$dbusername,$dbname,$dbpasword);
+      $conn = mysqli_connect($servername, $username, $password, $database);
 
   if($conn->connect_error){
     die("Connection failed: ". $conn->connect_error);
   }
 
-  $query = "SELECT *FROM employee WHERE email_id='$email' AND post='$post' AND password='$password'";
+  $query = "SELECT * FROM employee WHERE email_id='$email' AND post='$post' AND password='$password'";
   $result = $conn->query($query);
 
   if($result->num_rows == 1){
-    header("Location: emp.html");
-    exit();
+    switch ($post) {
+      case 'HR':
+          header('Location: hr_dashboard.php');
+          break;
+      case 'Manager':
+          header('Location: manager_dashboard.php');
+          break;
+      case 'Employee':
+          header('Location: employee_dashboard.php');
+          break;
+      }
   }
   else{
-    echo "<script>alert('Invalid Email or Password')</script>";
-    exit();
+      echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+      <strong>ERROR ⚠️</strong> Check Your Login Credentials !!
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+       <span aria-hidden="true">&times;</span>
+      </button>
+      </div>';
+    // header('Location: login.html');
   }
   $conn->close();
 }
